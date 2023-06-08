f581694501c86fd5a269a85cb44042198ee7d835
Hoàng Văn Tân <tan.hoang@skyblazestudios.com>
2023-06-09 00:48:07 +0700

    update deploy

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/api/v1/locations', locationRoutes);
 app.use('/api/v1/comments', commentRoutes);
 app.use('/api/v1/registered', registeredSalonRoutes);
 
-const buildPath = path.join(__dirname, "../salon-web/build");
+const buildPath = path.join(__dirname, "build");
 
 // Serve the static files from the React app
 app.use(express.static(buildPath));
@@ -49,6 +49,7 @@ app.use(express.static(buildPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
+
 // Start the server
 const port = process.env.PORT || 8600;
 app.listen(port, () => {
